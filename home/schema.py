ba53b41f1514e0e7d6e67fc74dfee559d6666bc2
amirbigg <amirbig44@gmail.com>
2021-01-22 06:23:24 -0500

    add ep9

--- home/schema.py
+++ home/schema.py
@@ -16,9 +16,23 @@ class CarType(DjangoObjectType):
 class HomeQuery(ObjectType):
 	persons = graphene.List(PersonType)
 	cars = graphene.List(CarType)
+	person = graphene.Field(PersonType, name=graphene.String())
+	car = graphene.Field(CarType, id=graphene.Int())
 
 	def resolve_persons(parent, info, **kwargs):
 		return Person.objects.all()
 
 	def resolve_cars(parent, info, **kwargs):
 		return Car.objects.all()
+
+	def resolve_person(parent, info, **kwargs):
+		name = kwargs.get('name')
+		if name is not None:
+			return Person.objects.get(name=name)
+		return None
+
+	def resolve_car(parent, info, **kwargs):
+		id = kwargs.get('id')
+		if id is not None:
+			return Car.objects.get(id=id)
+		return None
